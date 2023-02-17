ad26b28e57a85de3d4ce47b8279ae9399f0a8fb9
AloSamaa <kaizkiegt@gmail.com>
2023-02-17 11:34:29 +0800

    simple landing page

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -3,53 +3,61 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
-    const router = useRouter();
-    console.log(router.asPath);
+  const router = useRouter();
 
-    return (
-        <nav className="navbar navbar-expand-lg pt-5 bg-transparent">
-            <div className="container-fluid">
-                <a className="navbar-brand text-white ps-3 ms-lg-5 fw-bold" href="/">
-                    Gabriel Dayot
+  return (
+    <nav className="navbar navbar-expand-lg pt-5 bg-transparent">
+      <div className="container">
+        <a className="navbar-brand text-white ps-3 ms-lg-5 fw-bold" href="/">
+            Gabriel Dayot
+        </a>
+        <button
+          className="navbar-toggler me-3"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mx-auto ps-3">
+            <li className="nav-item">
+              <Link href="/" passHref legacyBehavior>
+                <a className={`nav-link navlink ${router.pathname === '/' ? 'text-danger' : 'text-white'}`}>
+                  Home
                 </a>
-                <button
-                    className="navbar-toggler me-3"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mx-auto ps-3">
-                        <li className="nav-item active">
-                            <Link href="/" className={`nav-link navlink ${router.asPath === '/' ? 'text-danger' : 'text-white'}`}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link href="/about" className={`nav-link text-white navlink ${router.asPath === '/about' ? 'text-danger' : 'text-white'}`}>
-                                About
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link href="/projects" className={`nav-link text-white navlink ${router.asPath === '/projects' ? 'text-danger' : 'text-white'}`}>
-                                Projects
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link href="/contact" className={`nav-link text-white navlink ${router.asPath === '/contact' ? 'text-danger' : 'text-white'}`}>
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/about" passHref legacyBehavior>
+                <a className={`nav-link navlink ${router.pathname === '/about' ? 'text-danger' : 'text-white'}`}>
+                  About
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/projects" passHref legacyBehavior>
+                <a className={`nav-link navlink ${router.pathname === '/projects' ? 'text-danger' : 'text-white'}`}>
+                  Projects
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/contact" passHref legacyBehavior>
+                <a className={`nav-link navlink ${router.pathname === '/contact' ? 'text-danger' : 'text-white'}`}>
+                  Contact
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import { Inter } from '@next/font/google'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,8 +8,25 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <div>
-      hello world
+      <div className="container pt-lg-5 mt-lg-5">
+        <div className="p-5 m-lg-5 col-md-6 card1">
+          <h3 className="fw-bold text-white">Gabriel Angelo D. Dayot</h3>
+          <span className="text-secondary pe-4">Fullstack Web Developer</span>
+          <span className="text-white">
+            <a className="atag" target="_blank" href="https://www.facebook.com/profile.php?id=100088506030945"> <FontAwesomeIcon icon={faFacebook} className="ps-2" width="27px"/> </a>
+            <a className="atag" target="_blank" href="https://github.com/AloSamaaa"> <FontAwesomeIcon icon={faLinkedin} className="ps-2" width="27px"/> </a>
+            <a className="atag" target="_blank" href="https://www.linkedin.com/in/gabriel-dayot-6a60071b2/"> <FontAwesomeIcon icon={faGithub} className="ps-2" width="27px"/> </a>
+          </span>
+          <br/>
+          <br/>
+          <span className="text-white">
+            As a fullstack web developer, I'm passionate about creating beautiful and functional web applications.
+            I take pride in my work and enjoy facing the challenges that come with building great software.
+            With each new project, I bring a creative and innovative approach, leveraging my skills and experience
+            to deliver quality solutions that exceed expectations</span>
+          <br/>
+          <br/>
+        </div>
       </div>
     </main>
   )
