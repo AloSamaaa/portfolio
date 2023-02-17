6ea12ec83e25585974987b8e8cca33ce7754e1ea
AloSamaa <kaizkiegt@gmail.com>
2023-02-17 18:24:39 +0800

    improvements

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return ( 
-        <footer className="text-start container">
-            <div className="mb-3">
+        <footer className="text-start container-fluid">
+            <div className="mb-3 ms-5 me-5">
                 <hr className="text-white"/>
                 <p className="text-white fw-bold">Gabriel <span className="lastName">Dayot.</span></p>
             </div>
