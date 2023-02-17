ce9a00a9a42635bce5026360497ace1135260c39
AloSamaa <kaizkiegt@gmail.com>
2023-02-17 11:45:59 +0800

    asd

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,7 +41,8 @@ export default function RootLayout({ children }) {
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-          crossorigin="anonymous">
+          crossorigin="anonymous"
+          async>
         </script>
         
         </body>
