6afe2bd9bf3776d312d15be2c303da38478df481
AloSamaa <kaizkiegt@gmail.com>
2023-02-17 12:40:51 +0800

    fade in

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <main>
       <div className="container pt-lg-5 mt-lg-5 pb-5 mb-5">
         <div className="p-5 m-lg-5 col-md-6 card1">
-          <h3 className="fw-bold text-white">Gabriel <span className="lastName">Dayot</span></h3>
+          <h3 className="fw-bold text-white">Gabriel Angelo <span className="lastName">D. Dayot</span></h3>
           <span className="text-secondary">Fullstack Web Developer</span>
           <div className="text-white mt-2 mb-3">
             <a className="atag" target="_blank" rel="noreferrer" href="https://www.facebook.com/profile.php?id=100088506030945"> <FontAwesomeIcon icon={faFacebook} className="ps-2" width="27px"/> </a>
@@ -21,7 +21,7 @@ export default function Home() {
             As a <span className="lastName">Fullstack web developer</span>, I&apos;m passionate about creating beautiful and functional web applications.
             I take pride in my work and enjoy facing the challenges that come with building great software.
             With each new project, I bring a creative and innovative approach, leveraging my skills and experience
-            to deliver quality solutions that exceed expectations</span>
+            to deliver quality solutions that exceed expectations.</span>
           <br/>
           <br/>
         </div>
