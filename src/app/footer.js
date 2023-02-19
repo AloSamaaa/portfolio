0e42beb166723bb6cb1ef51819db42b8be40a739
AloSamaa <kaizkiegt@gmail.com>
2023-02-19 09:47:33 +0800

    fix footer

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <footer className="text-start container-fluid position-absolute bottom-0 end-0">
+        <footer className="text-start container-fluid position-fixed bottom-0 end-0 mt-5">
             <div className="mb-3 ms-5 me-5">
                 <hr className="text-white"/>
                 <p className="text-white fw-bold">Gabriel <span className="lastName">Dayot.</span></p>
