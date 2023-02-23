b0c72c6b3a0f6654cac9b41346b520043d1c75ac
AloSamaa <kaizkiegt@gmail.com>
2023-02-23 11:04:07 +0800

    small cleaning

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Projects = () => {
     return (
@@ -13,31 +14,31 @@ const Projects = () => {
                         <div className="card card6">
                             <Image src="/gamefo.jpg" class="card-img-top" width={1920} height={250} alt="..." />
                             <div className="card-body text-center">
-                                <a href="https://alosamaa.vercel.app/" target="_blank" rel="noreferrer" className="btn button6">Live link</a>
-                                <a href="https://github.com/AloSamaaa/Major-project-1" target="_blank" rel="noreferrer" className="btn button6 ms-5">Github repo</a>
+                                <Link href="https://alosamaa.vercel.app/" target="_blank" rel="noreferrer" className="btn button6">Live link</Link>
+                                <Link href="https://github.com/AloSamaaa/Major-project-1" target="_blank" rel="noreferrer" className="btn button6 ms-5">Github repo</Link>
                             </div>
                         </div>
                     </div>
-                    
+
                     <div className="col-md-4">
                         <div className="card card6">
                             <Image src="/resortvillas.jpg" class="card-img-top" width={1920} height={250} alt="..." />
                             <div className="card-body text-center">
-                                <a href="https://resortvillas.vercel.app/" target="_blank" rel="noreferrer" className="btn button6">Live link</a>
-                                <a href="https://github.com/WD29Group/resortvillas" target="_blank" rel="noreferrer" className="btn button6 ms-5">Github repo</a>
+                                <Link href="https://resortvillas.vercel.app/" target="_blank" rel="noreferrer" className="btn button6">Live link</Link>
+                                <Link href="https://github.com/WD29Group/resortvillas" target="_blank" rel="noreferrer" className="btn button6 ms-5">Github repo</Link>
                             </div>
                         </div>
-                    </div>  
+                    </div>
 
                     <div className="col-md-4">
                         <div className="card card6">
                             <Image src="/dreamfields.jpg" class="card-img-top" width={1920} height={250} alt="..." />
                             <div className="card-body text-center">
-                                <a href="https://alosamaa.vercel.app/" target="_blank" rel="noreferrer" className="btn button6">Live link</a>
-                                <a href="https://github.com/AloSamaaa/Major-project-1" target="_blank" rel="noreferrer" className="btn button6 ms-5">Github repo</a>
+                                <Link href="https://alosamaa.vercel.app/" target="_blank" rel="noreferrer" className="btn button6">Live link</Link>
+                                <Link href="https://github.com/AloSamaaa/Major-project-1" target="_blank" rel="noreferrer" className="btn button6 ms-5">Github repo</Link>
                             </div>
                         </div>
-                    </div>    
+                    </div>
                 </div>
             </div>
         </section>
